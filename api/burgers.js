3efc6f1bc9c03725ccdde32e2a7cde2e48a6d2a4
SoliBlack <SolitaireBlack21@gmail.com>
2024-10-21 21:38:29 -0400

    Added Eggers Can't Be Cheesers Burger

--- api/burgers.js
+++ api/burgers.js
@@ -36,8 +36,31 @@ export default function handler(req, res) {
           ],
           image: 'https://res.cloudinary.com/dxbdiw4c7/image/upload/v1729401651/New_Bacon-ing_Burger_fonilp.png',
           servingSize: 4
-
-      }
+      },
+      {
+        name: "Eggers Can’t Be Cheesers Burger",
+        episode: "Season 3, Episode 11: Nude Beach",
+        description: "An all-beef patty topped with American cheese and a fried egg sunny-side up. Served with hot sauce and a piece of lettuce on a plain bun. It’s eggscatly as eggstraordinary as it sounds. Eggs.",
+        ingredients: [
+          "1 pound ground beef",
+          "8 slices American cheese",
+          "4 large eggs",
+          "4 English muffins",
+          "Green leaf lettuce",
+          "Hot sauce (optional, but better)",
+          "Butter"
+        ],
+        steps: [
+          "Make 4 patties, season both sides with salt and pepper, and cook the burgers. When they’re just about done, top with the cheese (2 slices per burger). Cover the pan if you’re cooking inside to help melt the cheese.",
+          "In a large frying pan, cook your eggs sunny-side up over a bit of butter. No flipping these guys—that’s what sunny side up means.",
+          "Toast your English muffins.",
+          "Build your burger: Spread a tiny bit of butter on your toasted English muffins, then bottom muffin, cheeseburger, and egg on top. Finish it with a dash of hot sauce, and the muffin top.",
+          "Grab a napkin—you’ll need it!"
+        ],
+        image: 'https://res.cloudinary.com/dxbdiw4c7/image/upload/v1729560932/lb0dmrxonsn5lbj6s09y.png',
+        servingSize: 4      
+      },
+      
   ];
 
   // Send the burger data as JSON
