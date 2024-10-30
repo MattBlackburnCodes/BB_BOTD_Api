f25f8ff1927a2be6675c18ea7fa81b0629919ef2
SoliBlack <SolitaireBlack21@gmail.com>
2024-10-29 21:40:43 -0400

    Added new burgers and images
    Added three new burgers and images
    
    Do the Brussel Burger
    Poblano Picasso Burger
    Totally Radish Burger

--- api/burgers.js
+++ api/burgers.js
@@ -77,14 +77,89 @@ export default function handler(req, res) {
         steps: [
 
           "Melt the butter in a wide frying pan over medium-low heat. Add the shallots and stir to coat. Cook over fairly low heat, stirring occasionally, until the shallots are very soft and a deep, sticky golden-brown, about 15 to 20 minutes.",
-          "Form 4 patties, season both sides with salt and pepper, and cook the burgers, melting a few slices of Manchego over each burger. Cov        steps: [er the pan or tent with foil to help the cheese melt.",
+          "Form 4 patties, season both sides with salt and pepper, and cook the burgers, melting a few slices of Manchego over each burger. Cover the pan or tent with foil to help the cheese melt.",
           "Build your burger: Bottom bun, then the cheeseburger, arugula, and sautéed shallots. Spread the fig jam on the top bun. Spread happiness in your face."
         ],
         image: "https://res.cloudinary.com/dxbdiw4c7/image/upload/v1730076992/A_Good_Manchego_is_Hard_to_Find_Burger_keup9i.png",
         servingSize: 4
+      },
+      {
+        name: "Totally Radish Burger",
+        episode: "Season 1, Episode 5: Hamburger Dinner Theater",
+        description: "An all-beef patty topped with a refreshingly crunchy-creamy-spicy mix of crème fraîche, cucumbers, dill, and radishes.",
+        ingredients: [
+          "1 cucumber, peeled",
+          "About 10-12 radishes",
+          "1/3 cup crème fraîche",
+          "1 tablespoon chopped fresh dill",
+          "1 pound ground beef",
+          "Green leaf lettuce",
+          "4 French rolls (plain buns are fine, too)"
+        ],
+        steps: [
+          "Use a microplane to grate the cucumber and the radishes into a medium bowl. Add the crème fraîche and the dill and mix.",
+          "Form 4 patties, season both sides with salt and pepper, and cook your burgers.",
+          "Build your burger: Put some lettuce and a burger on the bottom roll, some of that delicious radish-cucumber mixture, top bun.",
+          "Add the garnish. Did you forget? We just talked about it. You know what, it's fine. I don't know why I'm making such a big deal about it."
+        ],
+        image: "https://res.cloudinary.com/dxbdiw4c7/image/upload/v1730250715/Totally_Radish_Burger_wgiqd8.png",
+        servingSize: 4
+      },
+      {
+        name: "Do the Brussel Burger",
+        episode: "Season 2, Episode 3: Synchronized Swimming",
+        description: "A lightly seasoned bacon burger topped with sautéed sprouts and pistachios.",
+        ingredients: [
+          "1/2 pound bacon",
+          "1/3 pound Brussels sprouts, thinly sliced",
+          "1/4 cup shelled pistachios, roughly chopped",
+          "1 pound ground beef",
+          "4 buns",
+          "Green leaf lettuce",
+          "Sour cream"
+        ],
+        steps: [
+          "Cook your bacon on the stove in a large frying pan set over low heat. Cook the bacon until it's crispy, but not too crispy.",
+          "Transfer your bacon to paper towels, and throw your Brussels sprouts into that delicious bacon fat; increase the heat to medium-high.",
+          "Once the sprouts start to brown a bit, add the pistachios. Stir for about a minute, then remove from heat and set aside.",
+          "Season the beef with the salt and pepper, form 4 patties, and cook your burgers in the same pan.",
+          "Build your burger: Bottom bun, burger, bacon, Brussels sprouts mixture, a dollop of sour cream, and your top bun. Dollop. Dollop. That's a great word, dollop."
+        ],
+        image: "https://res.cloudinary.com/dxbdiw4c7/image/upload/v1730252153/Do_the_Brussel_Burger_zwvvhg.png",
+        servingSize: 4
+      },
+      {
+        name: "Poblano Picasso Burger",
+        episode: "Season 1, Episode 8: Art Crawl",
+        description: "An all-beef patty topped with a spicy poblano salsa verde, fresh tomatoes, and Monterey Jack cheese.",
+        ingredients: [
+          "1/4 cup chopped white onions",
+          "1 teaspoon butter",
+          "1 large poblano pepper, stemmed, halved lengthwise, and seeded",
+          "4 tomatillos, husked and quartered",
+          "1 jalapeño pepper",
+          "1 pound ground beef",
+          "4 slices Monterey Jack cheese",
+          "4 buns",
+          "Green leaf lettuce",
+          "1 large tomato, thickly sliced"
+        ],
+        steps: [
+          "Preheat your broiler.",
+          "Cook the onions in a small frying pan with butter over medium-high heat until translucent.",
+          "Put the poblano and tomatillos in a small casserole dish and place 6 inches under the broiler for 5 to 10 minutes, until the skins start to brown.",
+          "Add the cooked onions, broiled poblano and tomatillos, and the jalapeño to a food processor or blender, and puree; set aside.",
+          "Form 4 patties, season both sides with salt and pepper, and cook the burgers as you normally would, making sure to melt a heavy helping of Monterey Jack on top.",
+          "Build your burger: Bottom bun, cheeseburger, a generous helping of salsa verde, tomato, and the top bun."
+        ],
+        image: "https://res.cloudinary.com/dxbdiw4c7/image/upload/v1730252390/Poblano_Picasso_Burger_firekz.png",
+        servingSize: 4
+      }
       
       
-  }
+
+      
+
       
   ];
 
